901c41edd8a332b357f8438a5a6f100a5e94541e
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2022-08-05 10:56:07 +0200

    release_android: move check_local_changes to utils.sh
    This will be used by both commit-binaries.sh and release_android.sh so
    move it to the common utils.sh file.
    
    No functional change.
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- release_android.sh
+++ release_android.sh
@@ -10,18 +10,6 @@ source "${SRC}/commit-binaries.sh"
 
 PROJECTS_AIOT=("arm-trusted-firmware" "build" "optee-os" "ti-linux-firmware" "u-boot" "k3-image-gen")
 
-function check_local_changes {
-    local projects=("$@")
-
-    for project in "${projects[@]}"; do
-        pushd "${ROOT}/${project}"
-        if ! git diff-index --quiet HEAD; then
-            error_exit "Local changes detected in: ${project}"
-        fi
-        popd
-    done
-}
-
 function add_commit_msg {
     local -n commits_msg_ref="$1"
     local ti_config="$2"
