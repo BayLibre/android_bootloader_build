3da2ff421154194ee5c755ba53c62bcbe9f94bbf
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2023-07-10 15:07:03 +0200

    release scripts: drop k3-image-gen from logic
    Since k3-image-gen is replaced by binman, we no longer need to log the
    project sha-1 in our commit messages.
    
    Remove them.
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- commit-binaries.sh
+++ commit-binaries.sh
@@ -78,7 +78,7 @@ function commit_msg_body {
         pushd "${from_repo}/${project}"
         body+="- Project: ${project}:\n"
 
-        if [[ "${project}" != "ti-linux-firmware" && "${project}" != "k3-image-gen" ]]; then
+        if [[ "${project}" != "ti-linux-firmware" ]]; then
             remote_url=$(git remote get-url "${remote_name}")
         else
             remote_url=$(git remote get-url "ti")
