BayLibre/android_bootloader_build

commit 28fc14e3747f5a0a416a2094e84df36a3c12ecf5

config: add secure.gp config entry
The AM62Px does not have GP support (only HS-FS). Therefore, building the
bootloaders generate this error:

Image 'tiboot3-am62px-hs-evm.bin' is missing external blobs but is still functional: ti-fs-enc.bin sysfw-inner-cert
  OFCHK   .config
cp: cannot stat 'tiboot3-am62px-gp-evm.bin': No such file or directory

Introduce a new config field, gp, which tells whether we should copy the GP
bootloaders or not.

Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

release_android.sh
@@ -34,10 +34,13 @@ function add_commit_msg {
 function copy_binaries {
     local ti_out="$1"
     local ti_android_out="$2"
+    local gp=$(config_value "$1" secure.gp)
     local hsfs=$(config_value "$3" secure.hsfs)
     local mode="$4"
     if [[ "${mode}" == "debug" ]]; then
-        cp "${ti_out}/tiboot3-debug-gp.bin" "${ti_android_out}/tiboot3.bin"
+        if [[ "${gp}" == "True" ]]; then
+            cp "${ti_out}/tiboot3-debug-gp.bin" "${ti_android_out}/tiboot3.bin"
+        fi
         if [[ "${hsfs}" == "True" ]]; then
             cp "${ti_out}/tiboot3-debug-hsfs.bin" "${ti_android_out}/tiboot3-hsfs.bin"
         fi
@@ -46,7 +49,9 @@ function copy_binaries {
     fi
 
     if [[ "${mode}" == "release" ]]; then
-        cp "${ti_out}/tiboot3-release-gp.bin" "${ti_android_out}/tiboot3.bin"
+        if [[ "${gp}" == "True" ]]; then
+            cp "${ti_out}/tiboot3-release-gp.bin" "${ti_android_out}/tiboot3.bin"
+        fi
         if [[ "${hsfs}" == "True" ]]; then
             cp "${ti_out}/tiboot3-release-hsfs.bin" "${ti_android_out}/tiboot3-hsfs.bin"
         fi

build_tiboot.sh
@@ -20,6 +20,7 @@ function build_tiboot {
     local mode="${3:-release}"
     local out_dir=$(out_dir "$1" "${mode}")
     local soc=$(config_value "$1" tiboot3.soc)
+    local gp=$(config_value "$1" secure.gp)
     local hsfs=$(config_value "$1" secure.hsfs)
 
     local ti_defconfig=""
@@ -55,7 +56,10 @@ function build_tiboot {
     fi
     make -j"$(nproc)"
 
-    cp tiboot3-${soc}-gp-evm.bin "${out_dir}/tiboot3-${mode}-gp.bin"
+    if [[ "${gp}" == "True" ]]; then
+        echo "Copy GP binary "
+        cp tiboot3-${soc}-gp-evm.bin "${out_dir}/tiboot3-${mode}-gp.bin"
+    fi
     if [[ "${hsfs}" == "True" ]]; then
         echo "Copy HS-FS binary "
         cp tiboot3-${soc}-hs-fs-evm.bin "${out_dir}/tiboot3-${mode}-hsfs.bin"

config/boards/am625-beagleplay-dfu.yaml
@@ -22,4 +22,5 @@ android:
   binaries_path: vendor/ti/am62x/bootloader/am625-beagleplay-dfu
 
 secure:
+  gp: true
   hsfs: false

config/boards/am625-beagleplay.yaml
@@ -22,4 +22,5 @@ android:
   binaries_path: vendor/ti/am62x/bootloader/am625-beagleplay
 
 secure:
+  gp: true
   hsfs: false

config/boards/am62p-sk-dfu.yaml
@@ -21,4 +21,5 @@ android:
   binaries_path: vendor/ti/am62x/bootloader/am62px-sk-dfu
 
 secure:
+  gp: false
   hsfs: true

config/boards/am62p-sk.yaml
@@ -21,4 +21,5 @@ android:
   binaries_path: vendor/ti/am62x/bootloader/am62px-sk
 
 secure:
+  gp: false
   hsfs: true

config/boards/am62x-lp-sk-dfu.yaml
@@ -21,4 +21,5 @@ android:
   binaries_path: vendor/ti/am62x/bootloader/am62x-lp-sk-dfu
 
 secure:
+  gp: true
   hsfs: true

config/boards/am62x-lp-sk.yaml
@@ -21,4 +21,5 @@ android:
   binaries_path: vendor/ti/am62x/bootloader/am62x-lp-sk
 
 secure:
+  gp: true
   hsfs: true

commit 68ab44f2e9167b3621a24c4f99a1e2bc6446621a

release_android: fix tiboot3 gp copy to aosp
tiboot3.bin (the GP variant) is never copied to aosp because the gp variable is
never true.
In fact $1 is for ti_out and $3 is the config file.

Use $3 to fix the problem.

Fixes: 28fc14e3747f ("config: add secure.gp config entry")
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

release_android.sh
@@ -34,7 +34,7 @@ function add_commit_msg {
 function copy_binaries {
     local ti_out="$1"
     local ti_android_out="$2"
-    local gp=$(config_value "$1" secure.gp)
+    local gp=$(config_value "$3" secure.gp)
     local hsfs=$(config_value "$3" secure.hsfs)
     local mode="$4"
     if [[ "${mode}" == "debug" ]]; then